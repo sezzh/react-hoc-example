87415049f02221ea26569d5d4e749d2e107552f1
sezzh <sezzhltd@gmail.com>
2017-08-23 16:22:24 -0500

    working example of Proxy Property HOC

--- src/index.js
+++ src/index.js
@@ -4,5 +4,69 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Bebi extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      unValor: 'el valor'
+    }
+  }
+
+  bebiMethod = () => {
+    console.log('esto es bebi method');
+    return this.state.unValor
+  }
+
+  render () {
+    return (
+      <div>{this.props.title}</div>
+    )
+  }
+}
+
+class Main extends React.Component {
+    constructor (props) {
+    super(props)
+  }
+  render () {
+    return (
+      ppHOC(Bebi)
+    )
+  }
+}
+
+// HOC by PP Proxy Properties
+function ppHOC (WrappedComponent) {
+  return class Hocquito extends React.Component {
+    constructor (props) {
+      super(props)
+    }
+
+    getRef (componentInstance) {
+      this.componentChild = componentInstance
+      debugger
+      //this.componentChild.props.title = 'holi boli'
+    }
+
+    componentDidMount () {
+      console.log('componente montado')
+      console.log(this.componentChild)
+      this.title = {
+        title: 'un nuevo titulo'
+      }
+      this.forceUpdate()
+    }
+
+    render () {
+      const newProps = Object.assign({}, this.props, {ref: this.getRef.bind(this)})
+      return (
+        <WrappedComponent {...newProps} {...this.title} />
+      )
+    }
+  }
+}
+
+var Componente = ppHOC(Bebi)
+
+ReactDOM.render(<Componente title={'bebi'} />, document.getElementById('root'))
 registerServiceWorker();
